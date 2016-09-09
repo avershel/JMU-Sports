e61aeda341ade6975bf75da14b9546bf11fe0709
Austin Vershel <avershel@gmail.com>
2016-09-09 10:22:37 -0400

    Initial Commit

---
+++ JMU Sports/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JMU Sports
+//
+//  Created by Austin Vershel on 9/9/16.
+//  Copyright © 2016 Austin Vershel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JMU Sports/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JMU Sports
+//
+//  Created by Austin Vershel on 9/9/16.
+//  Copyright © 2016 Austin Vershel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
